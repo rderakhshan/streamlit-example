307097e6f9137163c3765f71180c11511af55682
rderakhshan <riemann.derakhshan@gmail.com>
2023-04-20 23:06:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,76 +2,83 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Define function to load Excel files and perform data analysis
-def load_data(filename):
-    df = pd.read_excel(filename)
-    # Perform data analysis here
-    return df
+# Define function to load data from selected files
+@st.cache(allow_output_mutation=True)
+def load_data(files):
+    data = []
+    for file in files:
+        df = pd.read_excel(file, engine='openpyxl')
+        data.append(df)
+    return data
 
-# Define function to create visualization on page 2
-def page_2(df):
-    st.header("Page 2")
-    # Create visualization using Plotly Express
-    fig = px.scatter(df, x="column1", y="column2")
+# Define function to create visualizations
+def create_viz(data):
+    fig = px.scatter(data, x='x', y='y', color='category')
+    return fig
+
+# Define pages
+def page_1():
+    st.title('Select Files')
+    files = st.sidebar.file_uploader('Select up to 6 Excel files', type='xlsx', accept_multiple_files=True)
+    if files:
+        data = load_data(files)
+        st.success('Data loaded successfully!')
+        st.sidebar.success('Files selected successfully!')
+        st.sidebar.markdown('---')
+        st.sidebar.markdown('## Navigation')
+        page = st.sidebar.radio("Go to", ("Page 2", "Page 3", "Page 4", "Page 5", "Page 6"))
+        if page == "Page 2":
+            page_2(data)
+        elif page == "Page 3":
+            page_3(data)
+        elif page == "Page 4":
+            page_4(data)
+        elif page == "Page 5":
+            page_5(data)
+        elif page == "Page 6":
+            page_6(data)
+
+def page_2(data):
+    st.title('Page 2')
+    fig = create_viz(data[0])
     st.plotly_chart(fig)
 
-# Define function to create visualization on page 3
-def page_3(df):
-    st.header("Page 3")
-    # Create visualization using Plotly Express
-    fig = px.line(df, x="column1", y="column2")
+def page_3(data):
+    st.title('Page 3')
+    fig = create_viz(data[1])
     st.plotly_chart(fig)
 
-# Define function to create visualization on page 4
-def page_4(df):
-    st.header("Page 4")
-    # Create visualization using Plotly Express
-    fig = px.bar(df, x="column1", y="column2")
+def page_4(data):
+    st.title('Page 4')
+    fig = create_viz(data[2])
     st.plotly_chart(fig)
 
-# Define function to create visualization on page 5
-def page_5(df):
-    st.header("Page 5")
-    # Create visualization using Plotly Express
-    fig = px.pie(df, values="column1", names="column2")
+def page_5(data):
+    st.title('Page 5')
+    fig = create_viz(data[3])
     st.plotly_chart(fig)
 
-# Define function to create visualization on page 6
-def page_6(df):
-    st.header("Page 6")
-    # Create visualization using Plotly Express
-    fig = px.histogram(df, x="column1", nbins=10)
+def page_6(data):
+    st.title('Page 6')
+    fig = create_viz(data[4])
     st.plotly_chart(fig)
 
-# Define function to create visualization on page 7
-def page_7(df):
-    st.header("Page 7")
-    # Create visualization using Plotly Express
-    fig = px.box(df, x="column1", y="column2")
-    st.plotly_chart(fig)
+# Define app
+def app():
+    page = st.sidebar.radio("Go to", ("Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6"))
+    if page == "Page 1":
+        page_1()
+    elif page == "Page 2":
+        page_2()
+    elif page == "Page 3":
+        page_3()
+    elif page == "Page 4":
+        page_4()
+    elif page == "Page 5":
+        page_5()
+    elif page == "Page 6":
+        page_6()
 
-# Define main function
-def main():
-    # Add file selector and uploader to sidebar
-    st.sidebar.title("Select a file to load")
-    file_option = st.sidebar.selectbox("Choose a file", ["data1.xlsx", "data2.xlsx", "data3.xlsx", "data4.xlsx", "data5.xlsx", "data6.xlsx", "data7.xlsx"])
-    uploaded_file = st.sidebar.file_uploader("Or upload your own file", type=["xlsx"])
-
-    # Load data on first page
-    st.sidebar.title("Pages")
-    st.sidebar.radio("Go to", ("Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6", "Page 7"))
-    
-    if file_option:
-        df = load_data(file_option)
-    elif uploaded_file:
-        df = load_data(uploaded_file)
-    else:
-        st.warning("Please select or upload a file.")
-        return
-
-    # Create pages using radio buttons
-    if st.sidebar.radio("Go to", ("Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6", "Page 7")) == "Page 1":
-        st.title("Page 1")
-        # Display loaded data
-        st.write(df)
-    elif st.sidebar.radio("Go to", ("Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6", "Page 7")) == "Page 2":
+# Run app
+if __name__ == '__main__':
+    app()
