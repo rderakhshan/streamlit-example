8a3ee2f2659c87feccbab3bceed7873ec30414e0
rderakhshan <riemann.derakhshan@gmail.com>
2023-04-20 23:25:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,15 @@ def page_1():
         data = load_data(files)
         st.success('Data loaded successfully!')
         st.write("<h2 style='text-align: center'>Navigation</h2>", unsafe_allow_html=True)
-        if st.button('Page 2', key='2'):
+        if st.button('Page 2', key='2', help='Go to Page 2', width=100, height=50, bg_color='#4CAF50', text_color='#FFFFFF'):
             page_2()
-        if st.button('Page 3', key='3'):
+        if st.button('Page 3', key='3', help='Go to Page 3', width=100, height=50, bg_color='#2196F3', text_color='#FFFFFF'):
             page_3()
-        if st.button('Page 4', key='4'):
+        if st.button('Page 4', key='4', help='Go to Page 4', width=100, height=50, bg_color='#f44336', text_color='#FFFFFF'):
             page_4()
-        if st.button('Page 5', key='5'):
+        if st.button('Page 5', key='5', help='Go to Page 5', width=100, height=50, bg_color='#9C27B0', text_color='#FFFFFF'):
             page_5()
-        if st.button('Page 6', key='6'):
+        if st.button('Page 6', key='6', help='Go to Page 6', width=100, height=50, bg_color='#FF9800', text_color='#FFFFFF'):
             page_6()
 
 def page_2():
@@ -46,19 +46,9 @@ def page_6():
 
 # Define app
 def app():
-    if st.sidebar.button('Page 1'):
+    st.sidebar.write("<h1 style='text-align: center; color: #FFFFFF; margin-top: 50px; margin-bottom: 50px;'>Navigation</h1>", unsafe_allow_html=True)
+    if st.sidebar.button('Page 1', key='1', help='Go to Page 1', width=150, height=75, bg_color='#3F51B5', text_color='#FFFFFF'):
         page_1()
-    if st.sidebar.button('Page 2'):
+    if st.sidebar.button('Page 2', key='2', help='Go to Page 2', width=150, height=75, bg_color='#4CAF50', text_color='#FFFFFF'):
         page_2()
-    if st.sidebar.button('Page 3'):
-        page_3()
-    if st.sidebar.button('Page 4'):
-        page_4()
-    if st.sidebar.button('Page 5'):
-        page_5()
-    if st.sidebar.button('Page 6'):
-        page_6()
-
-# Run app
-if __name__ == '__main__':
-    app()
+    if st.sidebar.button('Page 3', key='3', help='Go to Page 3', width=150, height=75, bg_color='#2196F3',
