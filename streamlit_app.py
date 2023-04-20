437d504f65722ecb7f4a578a983a94a76a8bebb5
rderakhshan <riemann.derakhshan@gmail.com>
2023-04-20 23:44:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,64 @@
 import streamlit as st
-import pandas as pd
+from streamlit_option_menu import option_menu
 
-# Define function to load data from selected files
-@st.cache(allow_output_mutation=True)
-def load_data(files):
-    data = []
-    for file in files:
-        df = pd.read_excel(file, engine='openpyxl')
-        data.append(df)
-    return data
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+EXAMPLE_NO = 1
 
-# Define pages
-def page_1():
-    st.write("<h1 style='text-align: center'>Select Files</h1>", unsafe_allow_html=True)
-    files = st.file_uploader('Select up to 6 Excel files', type='xlsx', accept_multiple_files=True)
-    if files:
-        data = load_data(files)
-        st.success('Data loaded successfully!')
-        st.write("<h2 style='text-align: center'>Navigation</h2>", unsafe_allow_html=True)
-        if st.button('Page 2', key='2'):
-            page_2()
-        if st.button('Page 3', key='3'):
-            page_3()
-        if st.button('Page 4', key='4'):
-            page_4()
-        if st.button('Page 5', key='5'):
-            page_5()
-        if st.button('Page 6', key='6'):
-            page_6()
 
-def page_2():
-    st.write("<h1 style='text-align: center'>Page 2</h1>", unsafe_allow_html=True)
+def streamlit_menu(example=1):
+    if example == 1:
+        # 1. as sidebar menu
+        with st.sidebar:
+            selected = option_menu(
+                menu_title="Main Menu",  # required
+                options=["Home", "Projects", "Contact"],  # required
+                icons=["house", "book", "envelope"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+            )
+        return selected
 
-def page_3():
-    st.write("<h1 style='text-align: center'>Page 3</h1>", unsafe_allow_html=True)
+    if example == 2:
+        # 2. horizontal menu w/o custom style
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+        )
+        return selected
 
-def page_4():
-    st.write("<h1 style='text-align: center'>Page 4</h1>", unsafe_allow_html=True)
+    if example == 3:
+        # 2. horizontal menu with custom style
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "25px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "green"},
+            },
+        )
+        return selected
 
-def page_5():
-    st.write("<h1 style='text-align: center'>Page 5</h1>", unsafe_allow_html=True)
 
-def page_6():
-    st.write("<h1 style='text-align: center'>Page 6</h1>", unsafe_allow_html=True)
+selected = streamlit_menu(example=EXAMPLE_NO)
 
-# Define app
-def app():
-    if st.sidebar.button('Page 1'):
-        page_1()
-    if st.sidebar.button('Page 2'):
-        page_2()
-    if st.sidebar.button('Page 3'):
-        page_3()
-    if st.sidebar.button('Page 4'):
-        page_4()
-    if st.sidebar.button('Page 5'):
-        page_5()
-    if st.sidebar.button('Page 6'):
-        page_6()
-
-# Run app
-if __name__ == '__main__':
-    app()
+if selected == "Home":
+    st.title(f"You have selected {selected}")
+if selected == "Projects":
+    st.title(f"You have selected {selected}")
+if selected == "Contact":
+    st.title(f"You have selected {selected}")
